3d1f7a0bd88e2f719c36b578ab31773ab7432f42
u <1550633318@qq.com>
2022-08-30 11:26:01 +0800

    second version

---
+++ 830drqv2-loss-all-2/env.sh
@@ -0,0 +1,2 @@
+conda env create -f conda_env.yml
+conda env update -f conda_env.yml
---
+++ 830drqv2-loss-all-2/scripts/run3.sh
@@ -0,0 +1,6 @@
+export MUJOCO_GL=osmesa
+export HYDRA_FULL_ERROR=1
+python train.py task=acrobot_swingup alpha=15
+
+
+
